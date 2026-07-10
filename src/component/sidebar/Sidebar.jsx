71f72dded5eab8e251ef55c871badd5a7b3a6414
iamezekiel <ezekieloghenerukevwe06@gmail.com>
2026-07-11 00:01:45 +0100

    my first commit

---
+++ src/component/sidebar/Sidebar.jsx
@@ -0,0 +1,14 @@
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <h2>Sidebar</h2>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "../../component/sidebar/Sidebar";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Welcome to the dashboard!</p>
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Dashboard;
